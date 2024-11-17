89f70894040d5ac4b8fcbe76e55162ab20de7d1b
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2024-11-17 09:52:44 +0000

    Added About and Contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
+import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <ProjectsSection />
+      <TapeSection />
+      <AboutSection />
+      <ContactSection />
     </div>
   );
 }
